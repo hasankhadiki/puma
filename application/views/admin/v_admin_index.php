b23b82801cfcdae2d45e7877d5633d38fe5083fd
Unknown <hasankhadiki@live.jp>
2017-12-10 06:09:00 +0700

    Progress-4 (Admin bisa nambah barang)
    Buka panel admin di, http://localhost/puma/dm_admin

---
+++ application/views/admin/v_admin_index.php
@@ -0,0 +1,170 @@
+
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Minimal an Admin Panel Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Minimal Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+<link href="<?php echo base_url('assets/css/admin_bootstrap.css');?>" rel='stylesheet' type='text/css' />
+<!-- <link href="<?php echo base_url();?>assets/css/bootstrap.css" rel='stylesheet' type='text/css' /> -->
+<!-- Custom Theme files -->
+<link href="<?php echo base_url('assets/css/admin_style.css');?>" rel='stylesheet' type='text/css' />
+<link href="<?php echo base_url('assets/css/admin_font-awesome.css');?>" rel="stylesheet"> 
+<script src="<?php echo base_url('assets/js/jquery.min.js');?>"> </script>
+<!-- Mainly scripts -->
+<script src="<?php echo base_url('assets/js/jquery.metisMenu.js');?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.slimscroll.min.js');?>"></script>
+<!-- Custom and plugin javascript -->
+<link href="<?php echo base_url('assets/css/admin_custom.css');?>" rel="stylesheet">
+<script src="<?php echo base_url('assets/js/custom.js');?>"></script>
+<script src="<?php echo base_url('assets/js/screenfull.js');?>"></script>
+		<script>
+		$(function () {
+			$('#supported').text('Supported/allowed: ' + !!screenfull.enabled);
+
+			if (!screenfull.enabled) {
+				return false;
+			}
+			
+
+			$('#toggle').click(function () {
+				screenfull.toggle($('#container')[0]);
+			});
+			
+		});
+		</script>
+<!--skycons-icons-->
+<script src="<?php echo base_url('assets/js/skycons.js');?>"></script>
+<!--//skycons-icons-->
+</head>
+<body>
+<div id="wrapper">
+
+
+        <nav class="navbar-default navbar-static-top" role="navigation">
+             <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+               <h1> <a class="navbar-brand" href="<?php echo base_url('index.php/dm_admin/index');?>">Puma Admin</a></h1>         
+			   </div>
+			 <div class=" border-bottom">
+        	<div class="full-left">
+        	  <section class="full-top">
+				<button id="toggle"><i class="fa fa-arrows-alt"></i></button>	
+			</section>
+            <div class="clearfix"> </div>
+           </div>
+     
+            <!-- Brand and toggle get grouped for better mobile display -->
+		 
+		   <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="drop-men" >
+		        <ul class=" nav_1">
+		        </ul>
+		     </div><!-- /.navbar-collapse -->
+			<div class="clearfix">
+       
+     </div>
+	  
+		    <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                <ul class="nav" id="side-menu">
+				
+                    <li>
+                        <a href="<?php echo base_url('index.php/dm_admin/index');?>" class=" hvr-bounce-to-right"><i class="fa fa-dashboard nav_icon "></i><span class="nav-label">Dashboards</span> </a>
+                    </li>
+                                        <li>
+                        <a href="<?php echo base_url('index.php/dm_admin/layout');?>" class=" hvr-bounce-to-right"><i class="fa fa-th nav_icon"></i> <span class="nav-label">Produk</span> </a>
+                    </li>
+                  
+                </ul>
+            </div>
+			</div>
+        </nav>
+        <div id="page-wrapper" class="gray-bg dashbard-1">
+       <div class="content-main">
+ 
+  		<!--banner-->	
+		    <div class="banner">
+		   
+				<h2>
+				<a href="<?php echo base_url('index.php/dm_admin/index');?>">Home</a>
+				<i class="fa fa-angle-right"></i>
+				<span>Dashboard</span>
+				</h2>
+		    </div>
+		<!--//banner-->
+
+	
+  
+		<div class="content-mid">
+			
+			<div class="col-md-12">
+					 <div class="cal1 cal_2"><div class="clndr"><div class="clndr-controls"><div class="clndr-control-button"><p class="clndr-previous-button">previous</p></div><div class="month">July 2015</div><div class="clndr-control-button rightalign"><p class="clndr-next-button">next</p></div></div><table class="clndr-table" border="0" cellspacing="0" cellpadding="0"><thead><tr class="header-days"><td class="header-day">S</td><td class="header-day">M</td><td class="header-day">T</td><td class="header-day">W</td><td class="header-day">T</td><td class="header-day">F</td><td class="header-day">S</td></tr></thead><tbody><tr><td class="day adjacent-month last-month calendar-day-2015-06-28"><div class="day-contents">28</div></td><td class="day adjacent-month last-month calendar-day-2015-06-29"><div class="day-contents">29</div></td><td class="day adjacent-month last-month calendar-day-2015-06-30"><div class="day-contents">30</div></td><td class="day calendar-day-2015-07-01"><div class="day-contents">1</div></td><td class="day calendar-day-2015-07-02"><div class="day-contents">2</div></td><td class="day calendar-day-2015-07-03"><div class="day-contents">3</div></td><td class="day calendar-day-2015-07-04"><div class="day-contents">4</div></td></tr><tr><td class="day calendar-day-2015-07-05"><div class="day-contents">5</div></td><td class="day calendar-day-2015-07-06"><div class="day-contents">6</div></td><td class="day calendar-day-2015-07-07"><div class="day-contents">7</div></td><td class="day calendar-day-2015-07-08"><div class="day-contents">8</div></td><td class="day calendar-day-2015-07-09"><div class="day-contents">9</div></td><td class="day calendar-day-2015-07-10"><div class="day-contents">10</div></td><td class="day calendar-day-2015-07-11"><div class="day-contents">11</div></td></tr><tr><td class="day calendar-day-2015-07-12"><div class="day-contents">12</div></td><td class="day calendar-day-2015-07-13"><div class="day-contents">13</div></td><td class="day calendar-day-2015-07-14"><div class="day-contents">14</div></td><td class="day calendar-day-2015-07-15"><div class="day-contents">15</div></td><td class="day calendar-day-2015-07-16"><div class="day-contents">16</div></td><td class="day calendar-day-2015-07-17"><div class="day-contents">17</div></td><td class="day calendar-day-2015-07-18"><div class="day-contents">18</div></td></tr><tr><td class="day calendar-day-2015-07-19"><div class="day-contents">19</div></td><td class="day calendar-day-2015-07-20"><div class="day-contents">20</div></td><td class="day calendar-day-2015-07-21"><div class="day-contents">21</div></td><td class="day calendar-day-2015-07-22"><div class="day-contents">22</div></td><td class="day calendar-day-2015-07-23"><div class="day-contents">23</div></td><td class="day calendar-day-2015-07-24"><div class="day-contents">24</div></td><td class="day calendar-day-2015-07-25"><div class="day-contents">25</div></td></tr><tr><td class="day calendar-day-2015-07-26"><div class="day-contents">26</div></td><td class="day calendar-day-2015-07-27"><div class="day-contents">27</div></td><td class="day calendar-day-2015-07-28"><div class="day-contents">28</div></td><td class="day calendar-day-2015-07-29"><div class="day-contents">29</div></td><td class="day calendar-day-2015-07-30"><div class="day-contents">30</div></td><td class="day calendar-day-2015-07-31"><div class="day-contents">31</div></td><td class="day adjacent-month next-month calendar-day-2015-08-01"><div class="day-contents">1</div></td></tr></tbody></table></div></div>
+			  <!----Calender -------->
+			<link rel="stylesheet" href="<?php echo base_url('assets/css/clndr.css');?>" type="text/css" />
+			<link rel="stylesheet" href="<?php echo base_url('assets/css/calendar.css');?>" type="text/css" />
+			<script src="<?php echo base_url();?>assets/js/underscore-min.js" type="text/javascript"></script>
+			<script src= "<?php echo base_url();?>assets/js/moment-2.2.1.js" type="text/javascript"></script>
+			<script src="<?php echo base_url();?>assets/js/clndr.js" type="text/javascript"></script>
+			<script src="<?php echo base_url();?>assets/js/site.js" type="text/javascript"></script>
+			<!----End Calender -------->
+			</div>
+			<div class="col-md-6">
+				
+		<div id="owl-demo" class="owl-carousel text-center">
+			
+		</div>
+		</div>
+
+		<link href="<?php echo base_url('assets/css/owl.carousel.css');?>" rel="stylesheet">
+		<script src="<?php echo base_url();?>assets/js/owl.carousel.js"></script>
+			<script>
+				$(document).ready(function() {
+					$("#owl-demo").owlCarousel({
+						items : 3,
+						lazyLoad : true,
+						autoPlay : true,
+						pagination : true,
+						nav:true,
+					});
+				});
+			</script>
+		
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+
+<div class="copy">
+            <p> &copy; 2016 Minimal. All Rights Reserved | Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+	    </div>
+		</div>
+		<div class="clearfix"> </div>
+       </div>
+     </div>
+<!---->
+<!--scrolling js-->
+	<script src="<?php echo base_url();?>assets/js/jquery.nicescroll.js"></script>
+	<script src="<?php echo base_url();?>assets/js/admin_scripts.js"></script>
+	<!--//scrolling js-->
+	<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"> </script>
+</body>
+</html>
+
